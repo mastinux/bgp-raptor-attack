4f424d0bf838194ad916e17929e7d61411058fe6
mastinux <andrea.pantaleo.93@gmail.com>
2019-06-15 09:36:51 -0700

    working star topology

--- bgp.py
+++ bgp.py
@@ -17,6 +17,11 @@ import os
 import termcolor as T
 import time
 
+NUM_ASES = 6
+NUM_HOSTS_PER_ASES = 3
+
+from utils import *
+
 setLogLevel('info')
 
 parser = ArgumentParser("Configure simple BGP network in Mininet.")
@@ -30,97 +35,98 @@ ROGUE_AS_NAME = 'R4'
 QUAGGA_STATE_DIR = '/var/run/quagga-1.2.4'
 
 def log(s, col="green"):
-    print T.colored(s, col)
+	print T.colored(s, col)
 
 
 class Router(Switch):
-    """Defines a new router that is inside a network namespace so that the
-    individual routing entries don't collide.
 
-    """
-    ID = 0
-    def __init__(self, name, **kwargs):
-        kwargs['inNamespace'] = True
-        Switch.__init__(self, name, **kwargs)
-        Router.ID += 1
-        self.switch_id = Router.ID
+	ID = 0
 
-    @staticmethod
-    def setup():
-        return
+	def __init__(self, name, **kwargs):
+		kwargs['inNamespace'] = True
+		Switch.__init__(self, name, **kwargs)
+		Router.ID += 1
+		self.switch_id = Router.ID
 
-    def start(self, controllers):
-        pass
+	@staticmethod
+	def setup():
+		return
 
-    def stop(self):
-        self.deleteIntfs()
+	def start(self, controllers):
+		pass
 
-    def log(self, s, col="magenta"):
-        print T.colored(s, col)
+	def stop(self):
+		self.deleteIntfs()
+
+	def log(self, s, col="magenta"):
+		print T.colored(s, col)
 
 
 class SimpleTopo(Topo):
-    """The Autonomous System topology is a simple straight-line topology
-    between AS1 -- AS2 -- AS3.  The rogue AS (AS4) connects to AS1 directly.
 
-    """
-    def __init__(self):
-        # Add default members to class.
-        super(SimpleTopo, self ).__init__()
-        num_hosts_per_as = 3
-        num_ases = 3
-        num_hosts = num_hosts_per_as * num_ases
-        # The topology has one router per AS
-	routers = []
-        for i in xrange(num_ases):
-            router = self.addSwitch('R%d' % (i+1))
-	    routers.append(router)
-        hosts = []
-        for i in xrange(num_ases):
-            router = 'R%d' % (i+1)
-            for j in xrange(num_hosts_per_as):
-                hostname = 'h%d-%d' % (i+1, j+1)
-                host = self.addNode(hostname)
-                hosts.append(host)
-                self.addLink(router, host)
+	def __init__(self):
+		# Add default members to class.
+		super(SimpleTopo, self ).__init__()
+		"""
+		num_hosts_per_as = 3
+		num_ases = 3
+		num_hosts = num_hosts_per_as * num_ases
+		"""
 
-        for i in xrange(num_ases-1):
-            self.addLink('R%d' % (i+1), 'R%d' % (i+2))
+		# The topology has one router per AS
+		routers = []
+		for i in xrange(NUM_ASES):
+			router = self.addSwitch('R%d' % (i+1))
+			routers.append(router)
 
-        routers.append(self.addSwitch('R4'))
-        for j in xrange(num_hosts_per_as):
-            hostname = 'h%d-%d' % (4, j+1)
-            host = self.addNode(hostname)
-            hosts.append(host)
-            self.addLink('R4', hostname)
-        # This MUST be added at the end
-        self.addLink('R1', 'R4')
-        return
+		hosts = []
+		for i in xrange(NUM_ASES):
+			router = 'R%d' % (i+1)
+
+			for j in xrange(NUM_HOSTS_PER_ASES):
+				hostname = 'h%d-%d' % (i+1, j+1)
+				host = self.addNode(hostname)
+				hosts.append(host)
+				self.addLink(router, host)
+
+		# adding links between ASes
+		self.addLink('R1', 'R2')
+		self.addLink('R1', 'R5')
+		self.addLink('R2', 'R3')
+		self.addLink('R2', 'R5')
+		self.addLink('R3', 'R4')
+		self.addLink('R3', 'R5')
+		self.addLink('R4', 'R5')
+		self.addLink('R4', 'R6')
+		self.addLink('R5', 'R6')
+
+		return
 
 
 def getIP(hostname):
-    AS, idx = hostname.replace('h', '').split('-')
-    AS = int(AS)
-    if AS == 4:
-        AS = 3
-    ip = '%s.0.%s.1/24' % (10+AS, idx)
-    return ip
+	AS, idx = hostname.replace('h', '').split('-')
+
+	AS = int(AS)
+
+	ip = '%s.%s.0.1/24' % (10+AS, idx)
+
+	return ip
 
 
 def getGateway(hostname):
-    AS, idx = hostname.replace('h', '').split('-')
-    AS = int(AS)
-    # This condition gives AS4 the same IP range as AS3 so it can be an
-    # attacker.
-    if AS == 4:
-        AS = 3
-    gw = '%s.0.%s.254' % (10+AS, idx)
-    return gw
+	AS, idx = hostname.replace('h', '').split('-')
+
+	AS = int(AS)
+
+	gw = '%s.%s.0.254' % (10+AS, idx)
+
+	return gw
 
 
 def startWebserver(net, hostname, text="Default web server"):
-    host = net.getNodeByName(hostname)
-    return host.popen("python webserver.py --text '%s' > /tmp/%s.log" % (text, hostname), shell=True)
+	host = net.getNodeByName(hostname)
+
+	return host.popen("python webserver.py --text '%s' > /tmp/%s.log" % (text, hostname), shell=True)
 
 
 def init_quagga_state_dir():
@@ -133,45 +139,53 @@ def init_quagga_state_dir():
 
 
 def main():
-    os.system("rm -f /tmp/R*.log /tmp/h*.log /tmp/*R*.pid logs/*stdout")
-    os.system("mn -c >/dev/null 2>&1")
-    os.system("killall -9 zebra bgpd > /dev/null 2>&1")
-    os.system('pgrep -f webserver.py | xargs kill -9')
+	os.system("rm -f /tmp/R*.log /tmp/h*.log /tmp/*R*.pid logs/*stdout")
+	os.system("mn -c >/dev/null 2>&1")
+	os.system("killall -9 zebra bgpd > /dev/null 2>&1")
+	os.system('pgrep -f webserver.py | xargs kill -9')
+	os.system('reset')
 
 	init_quagga_state_dir()
 
-    net = Mininet(topo=SimpleTopo(), switch=Router)
-    net.start()
-    for router in net.switches:
-        router.cmd("sysctl -w net.ipv4.ip_forward=1")
-        router.waitOutput()
+	net = Mininet(topo=SimpleTopo(), switch=Router)
+	net.start()
 
-    log("Waiting %d seconds for sysctl changes to take effect..."
-        % args.sleep)
-    sleep(args.sleep)
+	# configuring routers
+	for router in net.switches:
+		# 0 = no RPF
+		# 1 = RPF strict mode
+		# 2 = RPF loose mode
+		router.cmd("sysctl -w net.ipv4.conf.all.rp_filter=2")
 
-    for router in net.switches:
-        if router.name == ROGUE_AS_NAME and not FLAGS_rogue_as:
-            continue
-        router.cmd("~/quagga-1.2.4/zebra/zebra -f conf/zebra-%s.conf -d -i /tmp/zebra-%s.pid > logs/%s-zebra-stdout 2>&1" % (router.name, router.name, router.name))
-        router.waitOutput()
-        router.cmd("~/quagga-1.2.4/bgpd/bgpd -f conf/bgpd-%s.conf -d -i /tmp/bgp-%s.pid > logs/%s-bgpd-stdout 2>&1" % (router.name, router.name, router.name), shell=True)
-        router.waitOutput()
-        log("Starting zebra and bgpd on %s" % router.name)
+		router.cmd("sysctl -w net.ipv4.ip_forward=1")
+		router.waitOutput()
 
-    for host in net.hosts:
-        host.cmd("ifconfig %s-eth0 %s" % (host.name, getIP(host.name)))
-        host.cmd("route add default gw %s" % (getGateway(host.name)))
+	log2("sysctl changes to take effect", args.sleep, "cyan")
 
-    log("Starting web servers", 'yellow')
-    startWebserver(net, 'h3-1', "Default web server")
-    startWebserver(net, 'h4-1', "*** Attacker web server ***")
+	for router in net.switches:
+		router.cmd("~/quagga-1.2.4/zebra/zebra -f conf/zebra-%s.conf -d -i /tmp/zebra-%s.pid > logs/%s-zebra-stdout 2>&1" % (router.name, router.name, router.name))
+		router.waitOutput()
 
-    CLI(net)
-    net.stop()
-    os.system("killall -9 zebra bgpd")
-    os.system('pgrep -f webserver.py | xargs kill -9')
+		router.cmd("~/quagga-1.2.4/bgpd/bgpd -f conf/bgpd-%s.conf -d -i /tmp/bgp-%s.pid > logs/%s-bgpd-stdout 2>&1" % (router.name, router.name, router.name), shell=True)
+		router.waitOutput()
 
+		log("Starting zebra and bgpd on %s" % router.name)
+	
+	# configuring hosts
+	for host in net.hosts:
+		host.cmd("ifconfig %s-eth0 %s" % (host.name, getIP(host.name)))
+		host.cmd("route add default gw %s" % (getGateway(host.name)))
+
+	for i in xrange(NUM_ASES):
+		log("Starting web servers", 'yellow')
+		startWebserver(net, 'h%s-1' % (i+1), "Web server home page")
+
+	CLI(net)
+	net.stop()
+
+	os.system('pgrep zebra | xargs kill -9')
+	os.system('pgrep bgpd | xargs kill -9')
+	os.system('pgrep -f webserver.py | xargs kill -9')
 
 if __name__ == "__main__":
-    main()
+	main()
--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from argparse import ArgumentParser
 import sys
 import os
 import termcolor as T
-from subprocess import Popen, PIPE
+from subprocess import Popen, PIPE, check_output
 import re
 
 parser = ArgumentParser("Connect to a mininet node and run a command")
@@ -15,6 +15,11 @@ parser.add_argument('--cmd', default='ifconfig',
                     nargs="+",
                     help="Command to run inside node.")
 
+parser.add_argument('--snode',
+                    help="The source node's name (e.g., h1, h2, etc.)")
+parser.add_argument('--dnode',
+                    help="The destination node's name (e.g., h1, h2, etc.)")
+
 FLAGS = parser.parse_args()
 node_pat = re.compile(r'.*bash --norc --noediting -is mininet:(.*)')
 
@@ -25,7 +30,6 @@ def list_nodes(do_print=False):
     cmd = 'ps aux'
     proc = Popen(cmd.split(), stdout=PIPE)
     out, err = proc.communicate()
-
     # Mapping from name to pid.
     ret = {}
     for line in out.split('\n'):
@@ -45,19 +49,37 @@ def main():
         list_nodes(do_print=True)
         return
 
-    if not FLAGS.node:
+    if FLAGS.snode and FLAGS.dnode:
+        pid_by_name = list_nodes()
+
+        spid = pid_by_name.get(FLAGS.snode)
+        if spid is None:
+            print "node `%s' not found" % (FLAGS.snode)
+            sys.exit(1)
+
+        dpid = pid_by_name.get(FLAGS.dnode)
+        if dpid is None:
+            print "node `%s' not found" % (FLAGS.dnode)
+            sys.exit(1)
+
+        dnode_IP_address = check_output("mnexec -a %s %s" % (dpid, 'hostname -I'), shell=True)
+
+        cmd = ' '.join(FLAGS.cmd) + ' ' + dnode_IP_address
+
+        os.system("mnexec -a %s %s" % (spid, cmd))
+
+    elif FLAGS.node:
+        pid_by_name = list_nodes()
+        pid = pid_by_name.get(FLAGS.node)
+        if pid is None:
+            print "node `%s' not found" % (FLAGS.node)
+            sys.exit(1)
+
+        cmd = ' '.join(FLAGS.cmd)
+
+        os.system("mnexec -a %s %s" % (pid, cmd))
+    else:
         parser.print_help()
-        return
-
-    pid_by_name = list_nodes()
-    pid = pid_by_name.get(FLAGS.node)
-
-    if pid is None:
-        print "node `%s' not found" % (FLAGS.node)
-        sys.exit(1)
-
-    cmd = ' '.join(FLAGS.cmd)
-    os.system("mnexec -a %s %s" % (pid, cmd))
 
 
 if __name__ == '__main__':
